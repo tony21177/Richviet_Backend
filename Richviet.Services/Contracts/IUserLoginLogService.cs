c49788d467ec2722b8f0f481e3755d20141e61d3
unknown <tony211776@hotmail.com>
2020-11-14 23:49:45 +0800

    modify login and register flow

---
+++ Richviet.Services/Contracts/IUserLoginLogService.cs
@@ -0,0 +1,12 @@
+﻿using Frontend.DB.EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Richviet.Services.Contracts
+{
+    public interface IUserLoginLogService
+    {
+        void AddLoginLog(UserLoginLog userLogingLog);
+    }
+}
---
+++ Richviet.Services/Services/UserLoginService.cs
@@ -0,0 +1,27 @@
+﻿using Frontend.DB.EF.Models;
+using Microsoft.Extensions.Logging;
+using Richviet.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Richviet.Services.Services
+{
+    public class UserLoginService : IUserLoginLogService
+    {
+        private readonly ILogger logger;
+        private readonly GeneralContext dbContext;
+
+        public UserLoginService(ILogger<IUserLoginLogService> logger, GeneralContext dbContext)
+        {
+            this.logger = logger;
+            this.dbContext = dbContext;
+        }
+
+        public void AddLoginLog(UserLoginLog userLogingLog)
+        {
+            dbContext.UserLoginLog.Add(userLogingLog);
+            dbContext.SaveChanges();
+        }
+    }
+}
