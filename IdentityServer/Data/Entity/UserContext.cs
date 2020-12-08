c4a1e30e575bd3c3cecbfa2435c32555fbd40255
unknown <tony211776@hotmail.com>
2020-12-08 17:10:58 +0800

    add admin identity server

---
+++ IdentityServer/Data/Entity/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Entity
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Claims> UserClaims { get; set; }
+    }
+}
